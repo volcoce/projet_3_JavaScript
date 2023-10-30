dc6d34df1d9d2580cb99033bdb85a52b1bcab6b0
louernos <40470776+volcoce@users.noreply.github.com>
2023-10-30 09:34:21 +0100

    Recuperation Donnee Dynamique

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,10 +1,20 @@
-console.log("test");
 getWorks()
-async function getWorks(){
-    const response = await fetch('http://localhost:5678/api/works')
-    const works = await response.json()
-    console.log(works)
-    for(const work of works){
-        console.log(work)
+async function getWorks() {
+    const response = await fetch('http://localhost:5678/api/works');
+    const works = await response.json();
+    const galleryContainer = document.querySelector('.gallery');
+
+    for (const work of works) {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        const figcaption = document.createElement('figcaption');
+
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        galleryContainer.appendChild(figure);
     }
 }
